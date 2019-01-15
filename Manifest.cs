bdca438d55c82cc401d7a2d6f9b5a2222f765bbb
Peter Keating <peter@moov2.com>
2019-01-15 16:55:09 +0000

    Create theme using boilerplate template
    Boilerplate theme template is available at the link below:
    https://github.com/moov2/Moov2.OrchardCore.ThemeBoilerplate

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Events Theme",
+    Author = "Moov2 Ltd.",
+    Website = "https://moov2.com",
+    Version = "0.0.1",
+    Description = "Theme for events website"
+)]
